fec9309523a4d567693a1c9f77865f43fd8a9cdf
“crdiegobeltramo” <crdiegobeltramo@gmail.com>
2022-09-07 23:53:07 -0300

    Primer commit1

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.Scanner;
+/**
+ *
+ * @author diego
+ */
+
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //ejercicio video 1
+       
+       String nombre = "Diego";
+         
+         int edad;
+         
+         Scanner leer = new Scanner(System.in);
+         
+         System.out.println("Ingrese su edad");
+         
+         edad = leer.nextInt();
+         
+         System.out.println("Hola " + nombre + " tenes " + edad + " años");
+                        
+        }
+                        
+         
+         
+    }
+         
+                 
+         
+    
